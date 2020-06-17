61ab9e6e0c4d569683002fd34599772345db75b8
Ross MacPhee <developerross@gmail.com>
2020-06-18 00:14:24 +0100

    JSMNM1:- Create initial Angular library structure

---
+++ projects/jasminum/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Jasminum
+ *
+ * @remarks Decorators and utilities for a more expressive jasmine testing experience
+ * @author Ross MacPhee <hello@rossmacphee.com> (https://rossmacphee.com/)
+ * @see https://github.com/rossyman/jasminum
+ * @license MIT
+ */
+
